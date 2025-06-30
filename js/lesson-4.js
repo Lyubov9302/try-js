8eb247bddb1918c463be853ab8624eeaea4d19fd
Lyubov9302 <lyubovyurynets@gmail.com>
2025-06-30 15:55:52 +0300

    added js

--- js/lesson-4.js
+++ js/lesson-4.js
@@ -1,17 +1,5 @@
 
 
-// 10 - знайти елемент li який має атрибут data-topic з значенням "navigation" і виведи його в консоль;
-// 11 - додай для знайденого елемента data-topic="navigation" атрибут style і зроби його backgroundColor жовтим
-// 12 - у елемента data-topic="navigation" знайди елемент р і зміни його текст на "Я змінив тут текст!".
-// 13 - створи const currentTopic = "manipulation"; після цього знайди елемент у якогоо атрибут data-topic має значення, яке зберігається у змінній currentTopic і виведи його в консоль;
-// 14 - додай до знайденого елемента атрибут style і зроби його backgroundColor блакитним;
-// 15 - знайти в документі заголовок, який має class="completed" і виведи його в консоль;
-// 16 - видали елемент li в якому знаходиться заголовок, який має class="completed"
-// 17 - після заголовка h1 (перед списком) додай новий елемент p і задай йому наступний текст: "Об'єктна модель документа (Document Object Model)"
-// 18 - додай новий елемент списку у кінець списка, його заголовок це - "Властивість innerHTML" а опис (р) - "Ще один спосіб створити DOM-елементи і помістити їх в дерево - це використовувати рядки з тегами і дозволити браузеру зробити всю важку роботу". тобто, потрібно створити елемент LI потім наповнити H3 та P і готову LI закинути у кінець списку
-// 19 - зроби це саме, але використовуй шаблонні рядки та метод insertAdjacentHTML()
-// 20 - очисти список
-
 // 1 - отримай body елемент і виведи його в консоль;
 const body = document.body;
 console.log(`1:`, body);
@@ -56,13 +44,79 @@ const navLiEl = document.querySelector('[data-topic="navigation"]');
 console.log(`10:`, navLiEl);
 
 
+// 11 - додай для знайденого елемента data-topic="navigation" атрибут style і зроби його backgroundColor жовтим
+
+navLiEl.style.backgroundColor = 'yellow';
+
+// 12 - у елемента data-topic="navigation" знайди елемент р і зміни його текст на "Я змінив тут текст!".
+
+
+navLiEl.lastElementChild.textContent = "Я змінив тут текст!";
 
 
 
+// 13 - створи const currentTopic = "manipulation"; після цього знайди елемент 
+// у якого атрибут data-topic має значення,
+//  яке зберігається у змінній currentTopic і виведи його в консоль;
+
+
+const currentTopic = "manipulation";
+const topicLiEL = document.querySelector(`[data-topic = ${currentTopic}]`);
+console.log(`13:`, topicLiEL);
+
+
+// 14 - додай до знайденого елемента атрибут style і зроби його backgroundColor блакитним;
+
+topicLiEL.style.backgroundColor = 'blue';
+
+// 15 - знайти в документі заголовок, який має class="completed" і виведи його в консоль;
+
+const completedHeading = document.querySelector('.completed');
+console.log(`15:`, completedHeading);
+
+// 16 - видали елемент li в якому знаходиться заголовок, який має class="completed"
+
+completedHeading.parentNode.remove();
+
+// 17 - після заголовка h1 (перед списком) додай новий елемент p і задай йому наступний текст: "Об'єктна модель документа (Document Object Model)"
+
+const newP = document.createElement('p');
+newP.textContent = "Об'єктна модель документа (Document Object Model)";
+
+heading.after(newP);
+
+
+// 18 - додай новий елемент списку у кінець списка, його заголовок це - "Властивість innerHTML" 
+// а опис (р) - "Ще один спосіб створити DOM-елементи і помістити їх в дерево -
+//  це використовувати рядки з тегами і дозволити браузеру зробити всю важку роботу". тобто, 
+// потрібно створити елемент LI потім наповнити H3 та P і готову LI закинути у кінець списку
 
 
 
+const newLi = document.createElement('li');
+newLi.innerHTML = '<h3>Властивість innerHTML</h3><p>Ще один спосіб створити DOM-елементи і помістити їх в дерево - це використовувати рядки з тегами і дозволити браузеру зробити всю важку роботу</p>'
+list.append(newLi);
 
+
+
+// 19 - зроби це саме, але використовуй шаблонні рядки та метод insertAdjacentHTML()
+
+newLi.insertAdjacentHTML('beforeend', '<h3>Властивість innerHTML</h3><p>Ще один спосіб створити DOM-елементи і помістити їх в дерево - це використовувати рядки з тегами і дозволити браузеру зробити всю важку роботу</p>');
+
+// 20 - очисти список
+
+// list.remove();
+
+list.innerHTML= ' ';
+
+
+// Створіть контейнер div (з класом number-container) в HTML-документі 
+// та динамічно створіть 100 блоків (з класом number) наповнивши їх рандомними
+// числами від 1 до 100 і додайте їх до контейнера div(numberContainer). 
+// Парні числа повинні мати зелений фон (додати клас even), 
+// Непарні числа - жовтий фон (додати клас odd).
+
+// const randomNumber = () => Math.floor(Math.random() * 100) + 1;
 
 
 
